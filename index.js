06e2b44db8a024712cfe56dace9357077f560a5a
Rahul Thakur <rahul.thakur9985@gmail.com>
2022-03-13 05:06:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const csurf = require('csurf');
+const cookieParser = require('cookie-parser');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const csrfMiddleware = csurf({
+  cookie: true
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(csrfMiddleware);
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Hello World</h1>
+    <form action="/entry" method="POST">
+      <div>
+        <label for="message">Enter a message</label>
+        <input id="message" name="message" type="text" />
+      </div>
+      <input type="submit" value="Submit" />
+      <input type="hidden" name="_csrf" value="${req.csrfToken()}" />
+    </form>
+  `);
+});
+
+app.post('/entry', (req, res) => {
+  console.log(`Message received: ${req.body.message}`);
+  res.send(`CSRF token used: ${req.body._csrf}, Message received: ${req.body.message}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
